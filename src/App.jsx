c0749ff25ccae6b62436a7fe77a1332729548598
Pournima Gaikwad <pournima2496@gmail.com>
2023-08-18 17:47:13 +0530

    implemented redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./app/homeSlice";
 
 function App() {
+  const { url } = useSelector((state) => state.home);
+  console.log(url);
+  const dispatch = useDispatch();
+
   const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+      dispatch(getApiConfiguration(res));
+    });
   };
 
   useEffect(() => {
     apiTesting();
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <h1 style={{ color: "#fff" }}>{url?.total_pages}</h1>
+    </>
+  );
 }
 
 export default App;
