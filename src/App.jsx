aa9726d833775c8338053700e6ca4254b0191318
Pournima Gaikwad <pournima2496@gmail.com>
2023-08-18 18:02:08 +0530

    routing implementation done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { getApiConfiguration } from "./app/homeSlice";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details"
+import Explore from "./pages/explore/Explore"
+import SearchResult from "./pages/searchResult/SearchResult"
+import NotFound from "./pages/notFound/NotFound"
 
 function App() {
   const { url } = useSelector((state) => state.home);
@@ -20,9 +26,15 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h1 style={{ color: "#fff" }}>{url?.total_pages}</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/seach/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
