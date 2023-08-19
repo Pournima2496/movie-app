ab10480ea2f452a2aeff4e78c92f382e91c2684c
Pournima Gaikwad <pournima2496@gmail.com>
2023-08-19 20:40:41 +0530

    calling tmdb api's

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,46 @@ import { useSelector, useDispatch } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { getApiConfiguration } from "./app/homeSlice";
 import Home from "./pages/home/Home";
-import Details from "./pages/details/Details"
-import Explore from "./pages/explore/Explore"
-import SearchResult from "./pages/searchResult/SearchResult"
-import NotFound from "./pages/notFound/NotFound"
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import SearchResult from "./pages/searchResult/SearchResult";
+import NotFound from "./pages/notFound/NotFound";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
   const { url } = useSelector((state) => state.home);
   console.log(url);
   const dispatch = useDispatch();
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
+
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+      dispatch(getApiConfiguration(url));
     });
   };
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
   return (
     <BrowserRouter>
+      {/* <Header /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:mediaType/:id" element={<Details />} />
-        <Route path="/seach/:query" element={<SearchResult />} />
+        <Route path="/search/:query" element={<SearchResult />} />
         <Route path="/explore/:mediaType" element={<Explore />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
