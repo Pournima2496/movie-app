a6ec81256c95de3cb5f160abfd40ad30758aff08
Pournima Gaikwad <pournima2496@gmail.com>
2023-08-21 20:45:45 +0530

    page not found

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import "./notfound.scss"
+import React from "react";
+
+import "./notfound.scss";
+
+import ContentWrapper from "../../components/content/ContentWrapper";
 
 const NotFound = () => {
-  return (
-    <div>NotFound</div>
-  )
-}
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
-export default NotFound
+export default NotFound;
